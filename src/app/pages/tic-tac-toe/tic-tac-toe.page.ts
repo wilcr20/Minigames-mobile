7f728132823f429eb0c4bbe4d3717f56a3fb471b
Wilfred <wilcr20@gmail.com>
2024-11-10 20:00:52 -0600

    Detectting wining lines for tic tac toe

--- src/app/pages/tic-tac-toe/tic-tac-toe.page.ts
+++ src/app/pages/tic-tac-toe/tic-tac-toe.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Symbols } from './symbols.enum';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tic-tac-toe',
@@ -9,18 +10,25 @@ import { Symbols } from './symbols.enum';
 
 
 
-export class TicTacToePage  {
+export class TicTacToePage {
   generateVisualInterface = false;
-  grid: Array<any> = [];
+  hasWinner = false;
+  isTied = false;
+  grid: Array<Array<string>> = [];
   playerTurn: string = Math.random() > 0.5 ? Symbols.X : Symbols.O;
+  cellsWinner: Array<String> = [];
 
-  constructor() { 
+  constructor(
+    private router: Router
+  ) {
     this.createInitialGrid();
   }
 
-  createInitialGrid(){
+  createInitialGrid() {
     let rows = 3;
     let columns = 3;
+    this.cellsWinner = [];
+    this.grid = [];
     for (let i = 0; i < rows; i++) {
       this.grid.push([]);
       for (let j = 0; j < columns; j++) {
@@ -29,19 +37,154 @@ export class TicTacToePage  {
     }
   }
 
-  selectCell(row: number, column:number){
-    if(this.grid[row][column] == Symbols.NONE){
+  selectCell(row: number, column: number) {
+    // console.log(row, column)
+    if (this.grid[row][column] == Symbols.NONE && !this.hasWinner) {
       this.grid[row][column] = this.playerTurn;
-      this.playerTurn = this.getAnotherPlayerSymbol();
+      this.verifyPlayerWin();
     }
-    
+
   }
 
-  getAnotherPlayerSymbol(): string{
-    if(this.playerTurn == Symbols.O){
+  getAnotherPlayerSymbol(): string {
+    if (this.playerTurn == Symbols.O) {
       return Symbols.X;
     }
     return Symbols.O;
+  }
+
+  getEmptySpacesCount() {
+    let emptySpaces = 0;
+    for (let i = 0; i < this.grid.length; i++) {
+      for (let j = 0; j < this.grid[i].length; j++) {
+        if (this.grid[i][j] == Symbols.NONE) {
+          emptySpaces++;
+        }
+      }
+    }
+    return emptySpaces;
+  }
+
+  resetGame() {
+    this.hasWinner = false;
+    this.isTied = false;
+    this.createInitialGrid();
+    this.playerTurn = Math.random() > 0.5 ? Symbols.X : Symbols.O;
+  }
+
+  backToHomePage() {
+    this.router.navigateByUrl("home")
+  }
+
+  verifyPlayerWin() {
+
+    const areSameValuesForRow = (currentValue: string) => currentValue == this.playerTurn;
+
+    //Verify rows
+    if (this.grid[0].every(areSameValuesForRow)) {
+      this.displayWinnerGameResult(1);
+      return;
+    }
+    if (this.grid[1].every(areSameValuesForRow)) {
+      this.displayWinnerGameResult(2);
+      return;
+    }
+    if (this.grid[2].every(areSameValuesForRow)) {
+      this.displayWinnerGameResult(3);
+      return;
+    }
+
+    //Verify columns
+    if (this.grid[0][0] == this.playerTurn &&
+      this.grid[1][0] == this.playerTurn &&
+      this.grid[2][0] == this.playerTurn) {
+      this.displayWinnerGameResult(4);
+      return;
+    }
+    if (this.grid[0][1] == this.playerTurn &&
+      this.grid[1][1] == this.playerTurn &&
+      this.grid[2][1] == this.playerTurn) {
+      this.displayWinnerGameResult(5);
+      return;
+    }
+    if (this.grid[0][2] == this.playerTurn &&
+      this.grid[1][2] == this.playerTurn &&
+      this.grid[2][2] == this.playerTurn) {
+      this.displayWinnerGameResult(6);
+      return;
+    }
+
+    //Verify diagonally
+    if (this.grid[0][0] == this.playerTurn &&
+      this.grid[1][1] == this.playerTurn &&
+      this.grid[2][2] == this.playerTurn) {
+      this.displayWinnerGameResult(7);
+      return;
+    }
+    if (this.grid[0][2] == this.playerTurn &&
+      this.grid[1][1] == this.playerTurn &&
+      this.grid[2][0] == this.playerTurn) {
+      this.displayWinnerGameResult(8);
+      return;
+    }
+
+    if(this.getEmptySpacesCount() == 0){
+      this.isTied = true;
+      return;
+    }
+
+    this.playerTurn = this.getAnotherPlayerSymbol();
+
+  }
+
+  displayWinnerGameResult(caseId: number) {
+    this.hasWinner = true;
+    this.cellsWinner = [];
+    switch (caseId) {
+      case 1:
+        this.cellsWinner.push("0,0");
+        this.cellsWinner.push("0,1");
+        this.cellsWinner.push("0,2");
+        break;
+      case 2:
+        this.cellsWinner.push("1,0");
+        this.cellsWinner.push("1,1");
+        this.cellsWinner.push("1,2");
+        break;
+      case 3:
+        this.cellsWinner.push("2,0");
+        this.cellsWinner.push("2,1");
+        this.cellsWinner.push("2,2");
+        break;
+      case 4:
+        this.cellsWinner.push("0,0");
+        this.cellsWinner.push("1,0");
+        this.cellsWinner.push("2,0");
+        break;
+      case 5:
+        this.cellsWinner.push("0,1");
+        this.cellsWinner.push("1,1");
+        this.cellsWinner.push("2,1");
+        break;
+      case 6:
+        this.cellsWinner.push("0,2");
+        this.cellsWinner.push("1,2");
+        this.cellsWinner.push("2,2");
+        break;
+      case 7:
+        this.cellsWinner.push("0,0");
+        this.cellsWinner.push("1,1");
+        this.cellsWinner.push("2,2");
+        break;
+      case 8:
+        this.cellsWinner.push("0,2");
+        this.cellsWinner.push("1,1");
+        this.cellsWinner.push("2,0");
+        break;
+      default:
+        break;
+    }
+
   }
 
 }
